6dd1098ef3ae2ca1db2c0155dfbc3d21d110810c
safwanvk <safwanvalakundil@gmail.com>
2020-07-11 18:37:59 +0530

    :lipstic: Add template video

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from core.views import ChannelView, CreateChannelView, HomeView
 
+
 urlpatterns = [
     path('<user>/channel/', ChannelView.as_view(), name='channel'),
     path('create-channel/', CreateChannelView.as_view(), name='create_channel'),
