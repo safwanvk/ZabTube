8045378cd012a7ab02b1e86cdab99edf4cecd23c
safwanvk <safwanvalakundil@gmail.com>
2020-07-11 15:42:41 +0530

    Add models Video and Comment

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+    path = models.CharField(max_length=60)
+    datetime = models.DateTimeField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=300)
+    datetime = models.DateTimeField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
