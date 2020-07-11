fa9d58a4dedeccae94ec648f6e1913a485ce9b31
safwanvk <safwanvalakundil@gmail.com>
2020-07-11 18:23:25 +0530

    :white_check_mark: Set view CrateChannel

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from core.views import ChannelView
+from core.views import ChannelView, CreateChannelView
 
 urlpatterns = [
-
-    path('<user>/channel', ChannelView.as_view())
+    path('<user>/channel/', ChannelView.as_view(), name='channel'),
+    path('create-channel/', CreateChannelView.as_view(), name='create_channel')
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.base import View
 
+from core.forms import ChannelForm
 from core.models import Video, Channel
 
 
@@ -16,3 +18,31 @@ class ChannelView(View):
             context = {'videos': videos,
                        'channel': channel}
         return render(request, self.template_name, context)
+
+
+class CreateChannelView(View):
+    template_name = "channel_create.html"
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            try:
+                if Channel.objects.filter(user__username=request.user).get().channel_name != "":
+                    return HttpResponseRedirect('/')
+            except Channel.DoesNotExist:
+                form = ChannelForm()
+                channel = False
+                return render(request, self.template_name, {'form': form, 'channel': channel})
+
+    def post(self, request):
+        # pass filled out HTML-Form from View to RegisterForm()
+        form = ChannelForm(request.POST)
+        if form.is_valid():
+            # create a User account
+            print(form.cleaned_data['channel_name'])
+            channel_name = form.cleaned_data['channel_name']
+            user = request.user
+            subscribers = 0
+            new_channel = Channel(channel_name=channel_name, user=user, subscribers=subscribers)
+            new_channel.save()
+            return HttpResponseRedirect('/')
+        return HttpResponse('This is Register view. POST Request.')
