5a7278592d5ada2f3c0a88397fbded95a3dfd1cc
safwanvk <safwanvalakundil@gmail.com>
2020-07-11 20:37:43 +0530

    :lipstic: Add template register

--- youtube/settings.py
+++ youtube/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'accounts'
+    'accounts',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -125,4 +126,5 @@ STATIC_FILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
