03d9502c6fca3b3bf74b7e2ad5fcc1ae8b33ddb9
safwanvk <safwanvalakundil@gmail.com>
2020-07-11 18:33:17 +0530

    Add form CommentForm

--- core/forms.py
+++ core/forms.py
@@ -3,3 +3,7 @@ from django import forms
 
 class ChannelForm(forms.Form):
     channel_name = forms.CharField(max_length=50, label='channel')
+
+
+class CommentForm(forms.Form):
+    text = forms.CharField(label='text', max_length=300)
