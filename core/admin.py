0390994109420d05a00b098559ea7260ebc0460c
safwanvk <safwanvalakundil@gmail.com>
2020-07-11 16:31:14 +0530

    Add model Channel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Video, Comment, Channel
+
+admin.site.register(Video)
+admin.site.register(Comment)
+admin.site.register(Channel)
--- core/models.py
+++ core/models.py
@@ -11,9 +11,21 @@ class Video(models.Model):
     datetime = models.DateTimeField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     text = models.TextField(max_length=300)
     datetime = models.DateTimeField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+
+class Channel(models.Model):
+    channel_name = models.CharField(max_length=50, blank=False, null=False)
+    subscribers = models.IntegerField(default=0, blank=False, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
