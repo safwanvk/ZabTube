53c6ca20c976fe6e18eff52cafc9905e787fd691
safwanvk <safwanvalakundil@gmail.com>
2020-07-11 19:03:06 +0530

    Add form NewVideoForm

--- core/forms.py
+++ core/forms.py
@@ -7,3 +7,9 @@ class ChannelForm(forms.Form):
 
 class CommentForm(forms.Form):
     text = forms.CharField(label='text', max_length=300)
+
+
+class NewVideoForm(forms.Form):
+    title = forms.CharField(label='Title', max_length=20)
+    description = forms.CharField(label='Description', max_length=300)
+    file = forms.FileField()
