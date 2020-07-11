4d1a35190210f29403b221fd2adcba6f8c578ee2
safwanvk <safwanvalakundil@gmail.com>
2020-07-11 20:47:03 +0530

    :white_check_mark: Set view register_view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+
+from accounts.views import register_view
+
 app_name = 'accounts'
 
 urlpatterns = [
+    path('register', register_view, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse
+
+from accounts.forms import UserCreationForm
+
+
+def register_view(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('accounts:login'))
+    return render(request, 'register.html', {'form': form})
